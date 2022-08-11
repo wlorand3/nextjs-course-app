ec704a850e940e2c5982a5bdece3fddd6ccaa8d3
Will Lorand <wlorand@cisco.com>
2022-08-10 19:15:26 -0700

    feat: did code changes for start

---
+++ pages/notes/[id].jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+const Note = () => {
+
+    const router = useRouter()
+    const { id } = router.query; 
+    
+    return (
+        <h1> Note {id} </h1> 
+    )
+}
+
+export default Note
---
+++ pages/notes/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Notes = () => <div>Notes Index Path</div>
+
+export default Notes
